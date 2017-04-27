e0659488bc4ccb79c09994e69af5cd9150e267fe
ms2sato <ms2.sato@gmail.com>
2017-04-28 08:13:56 +0900

    stop send rollbar on status 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -80,7 +80,7 @@ class ApplicationController < ActionController::Base
   def process_on_productions(exception, status_code)
     logger.warn("original_fullpath:#{request.original_fullpath}")
 
-    if ENV['ROLLBAR_ACCESS_TOKEN']
+    if ENV['ROLLBAR_ACCESS_TOKEN'] && status_code != 404
       # エラー通知
       logger.warn('send notify_exception')
       Rollbar.error(exception, env: request.env)
