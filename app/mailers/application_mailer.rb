9bb1b99b38de19e246884d219791aeb84ef2c36b
ms2sato <ms2.sato@gmail.com>
2017-04-30 19:53:23 +0900

    cut invalid mail address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -4,4 +4,9 @@ class ApplicationMailer < ActionMailer::Base
   default from: ENV.fetch('SMTP_FROM_ADDRESS') { 'notifications@localhost' }
   layout 'mailer'
   helper :instance
+
+  def mail(headers = {}, &block)
+    return puts "email not found: #{headers}" if headers[:to].end_with?('@github')
+    super(headers) { |format| block.call(format) if block.present? }
+  end
 end
